5b15a6bf729432e8d5efb06e6677b5ef7ce65b73
Elmi Aden <waraha01@gmail.com>
2024-11-24 13:17:40 -0800

    update to main.py
    fixing issues on sign in

--- main.py
+++ main.py
@@ -34,9 +34,11 @@ def root():
     with sqlite3.connect('database.db') as conn:
         cur = conn.cursor()
         # Show last product added
-        cur.execute('SELECT productId, name, price, description, image, stock FROM products ORDER BY productId DESC LIMIT 1 ')
+        cur.execute(
+            'SELECT productId, name, price, description, image, stock FROM products ORDER BY productId DESC LIMIT 1 ')
         # Show all items
         #cur.execute('SELECT productId, name, price, description, image, stock FROM products LIMIT 1')
+
         item_data = cur.fetchall()
         # Show an error instead of the categories
         category_data = [(-1,"Error")]
